105163e1de0b25393f0219d0d06e520aab8ffce3
AhoyISki <ahoyiski@gmail.com>
2023-09-12 15:14:31 -0300

    Fixed an inverted order here.

--- parsec-term/src/rules.rs
+++ parsec-term/src/rules.rs
@@ -1,6 +1,6 @@
 use parsec_core::{
     data::{AsAny, ReadableData, RoData},
-    forms::{FormId, FormPalette, DEFAULT},
+    forms::{Form, FormId},
     input::InputMethod,
     text::{BuilderTag, Text, TextBuilder},
     ui::{Area, PushSpecs, Ui},
@@ -11,7 +11,7 @@ use parsec_core::{
 /// The [`char`]s that should be printed above, equal to, and below
 /// the main line.
 #[derive(Clone)]
-pub enum SepChar {
+enum SepChar {
     Uniform(char),
     /// Order: main line, other lines.
     TwoWay(char, char),
@@ -31,34 +31,10 @@ impl SepChar {
     }
 }
 
-/// The `form_id`s that should be printed above, equal to, and below
-/// the main line.
-#[derive(Clone)]
-pub enum SepForm {
-    Uniform(FormId),
-    /// Order: main line, other lines.
-    TwoWay(FormId, FormId),
-    /// Order: main line, above main line, below main line.
-    ThreeWay(FormId, FormId, FormId),
-}
-
-impl SepForm {
-    /// The `form_id`s above, equal to, and below the main line,
-    /// respectively.
-    fn forms(&self) -> [FormId; 3] {
-        match self {
-            SepForm::Uniform(uniform) => [*uniform, *uniform, *uniform],
-            SepForm::TwoWay(main, other) => [*other, *main, *other],
-            SepForm::ThreeWay(main, lower, upper) => [*upper, *main, *lower],
-        }
-    }
-}
-
 /// The configurations for the [`VertRule`] widget.
 #[derive(Clone)]
 pub struct VertRuleCfg {
     sep_char: SepChar,
-    sep_form: SepForm,
     specs: PushSpecs,
 }
 
@@ -67,7 +43,6 @@ impl VertRuleCfg {
     pub fn new() -> Self {
         Self {
             sep_char: SepChar::Uniform('│'),
-            sep_form: SepForm::Uniform(DEFAULT),
             specs: PushSpecs::left().with_lenght(1.0),
         }
     }
@@ -119,50 +94,6 @@ impl VertRuleCfg {
         }
     }
 
-    pub fn with_form(self, palette: &FormPalette, form: impl AsRef<str>) -> Self {
-        let (_, form) = palette.from_name(form);
-        Self {
-            sep_form: SepForm::Uniform(form),
-            ..self
-        }
-    }
-
-    pub fn with_main_form(self, palette: &FormPalette, main: impl AsRef<str>) -> Self {
-        let (_, main) = palette.from_name(main);
-        Self {
-            sep_form: match self.sep_form {
-                SepForm::Uniform(other) => SepForm::TwoWay(main, other),
-                SepForm::TwoWay(_, other) => SepForm::TwoWay(main, other),
-                SepForm::ThreeWay(_, above, below) => SepForm::ThreeWay(main, above, below),
-            },
-            ..self
-        }
-    }
-
-    pub fn with_form_above(self, palette: &FormPalette, above: impl AsRef<str>) -> Self {
-        let (_, above) = palette.from_name(above);
-        Self {
-            sep_form: match self.sep_form {
-                SepForm::Uniform(other) => SepForm::ThreeWay(other, above, other),
-                SepForm::TwoWay(main, below) => SepForm::ThreeWay(main, above, below),
-                SepForm::ThreeWay(main, _, below) => SepForm::ThreeWay(main, above, below),
-            },
-            ..self
-        }
-    }
-
-    pub fn with_form_below(self, palette: &FormPalette, below: impl AsRef<str>) -> Self {
-        let (_, below) = palette.from_name(below);
-        Self {
-            sep_form: match self.sep_form {
-                SepForm::Uniform(other) => SepForm::ThreeWay(other, other, below),
-                SepForm::TwoWay(main, above) => SepForm::ThreeWay(main, above, below),
-                SepForm::ThreeWay(main, above, _) => SepForm::ThreeWay(main, above, below),
-            },
-            ..self
-        }
-    }
-
     pub fn builder<U: Ui>(
         self,
     ) -> impl FnOnce(&Controler<U>) -> (Widget<U>, Box<dyn Fn() -> bool>, PushSpecs) {
@@ -170,10 +101,15 @@ impl VertRuleCfg {
             let file = controler.current_file();
             let input = controler.current_input();
 
+            let palette = controler.palette();
+            let main = palette.set_new_form("VertRule", Form::new().grey());
+            let upper = palette.set_new_ref("UpperVertRule", "VertRule");
+            let lower = palette.set_new_ref("LowerVertRule", "VertRule");
+
             let builder = {
-                let file = file.read();
                 let input = input.read();
-                setup_builder(&file, &*input, &self)
+                let file = file.read();
+                setup_builder(&file, &*input, &self, (upper, main, lower))
             };
 
             let vert_rule = VertRule {
@@ -181,7 +117,7 @@ impl VertRuleCfg {
                 input: input.clone(),
                 builder,
                 sep_char: self.sep_char,
-                sep_form: self.sep_form,
+                forms: (upper, main, lower),
             };
 
             let checker = Box::new(move || file.has_changed() || input.has_changed());
@@ -205,7 +141,7 @@ pub struct VertRule {
     input: RoData<dyn InputMethod>,
     builder: TextBuilder,
     sep_char: SepChar,
-    sep_form: SepForm,
+    forms: (FormId, FormId, FormId),
 }
 
 impl VertRule {
@@ -230,23 +166,22 @@ impl PassiveWidget for VertRule {
     }
 
     fn update(&mut self, _area: &impl Area) {
-        let file = self.file.read();
-        let lines = file.printed_lines();
-        let builder = &mut self.builder;
-
         let main_line = {
             let input = self.input.read();
             input.cursors().unwrap().main().true_line()
         };
 
+        let file = self.file.read();
+        let lines = file.printed_lines();
+        let builder = &mut self.builder;
+
         let above = lines.iter().filter(|&(line, _)| *line < main_line).count();
         let equal = lines.iter().filter(|&(line, _)| *line == main_line).count();
         let below = lines.iter().filter(|&(line, _)| *line > main_line).count();
 
-        let forms = self.sep_form.forms();
         let chars = self.sep_char.chars();
 
-        builder.swap_tag(0, BuilderTag::PushForm(forms[0]));
+        builder.swap_tag(0, BuilderTag::PushForm(self.forms.0));
         builder.swap_range(
             0,
             [chars[0], '\n']
@@ -254,7 +189,7 @@ impl PassiveWidget for VertRule {
                 .collect::<String>()
                 .repeat(above),
         );
-        builder.swap_tag(1, BuilderTag::PushForm(forms[1]));
+        builder.swap_tag(1, BuilderTag::PushForm(self.forms.1));
         builder.swap_range(
             1,
             [chars[1], '\n']
@@ -262,7 +197,7 @@ impl PassiveWidget for VertRule {
                 .collect::<String>()
                 .repeat(equal),
         );
-        builder.swap_tag(2, BuilderTag::PushForm(forms[2]));
+        builder.swap_tag(2, BuilderTag::PushForm(self.forms.2));
         builder.swap_range(
             2,
             [chars[2], '\n']
@@ -278,7 +213,12 @@ impl PassiveWidget for VertRule {
 }
 
 /// Sets up a new [`TextBuilder<U>`] for the [`VertRule`] widget.
-fn setup_builder(file: &FileWidget, input: &dyn InputMethod, cfg: &VertRuleCfg) -> TextBuilder {
+fn setup_builder(
+    file: &FileWidget,
+    input: &dyn InputMethod,
+    cfg: &VertRuleCfg,
+    forms: (FormId, FormId, FormId),
+) -> TextBuilder {
     let lines = file.printed_lines();
 
     let main_line = input.cursors().unwrap().main().true_line();
@@ -293,19 +233,18 @@ fn setup_builder(file: &FileWidget, input: &dyn InputMethod, cfg: &VertRuleCfg) 
         .skip_while(|&(line, _)| *line <= main_line)
         .count();
 
-    let forms = cfg.sep_form.forms();
     let chars = cfg.sep_char.chars();
 
-    builder.push_tag(BuilderTag::PushForm(forms[0]));
+    builder.push_tag(BuilderTag::PushForm(forms.0));
     builder.push_swappable(
         [chars[0], '\n']
             .into_iter()
             .collect::<String>()
             .repeat(upper),
     );
-    builder.push_tag(BuilderTag::PushForm(forms[1]));
+    builder.push_tag(BuilderTag::PushForm(forms.1));
     builder.push_swappable([chars[1], '\n'].into_iter().collect::<String>());
-    builder.push_tag(BuilderTag::PushForm(forms[2]));
+    builder.push_tag(BuilderTag::PushForm(forms.2));
     builder.push_swappable(
         [chars[2], '\n']
             .into_iter()
