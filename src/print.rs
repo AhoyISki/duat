5366a99ff533c00464a0b38af7619e9a679735cb
AhoyISki <ahoyiski@gmail.com>
2024-07-16 21:47:33 -0300

    Somehow forgot this file.

---
+++ src/print.rs
@@ -0,0 +1,115 @@
+use std::ops::RangeInclusive;
+
+use duat_core::text::PrintCfg;
+
+use crate::setup::PRINT_CFG;
+
+#[inline(never)]
+pub fn no_wrapping() {
+    let mut print_cfg = PRINT_CFG.write().unwrap();
+    let prev = print_cfg.take();
+
+    *print_cfg = Some(match prev {
+        Some(prev) => prev.with_no_wrapping(),
+        None => PrintCfg::default().with_no_wrapping(),
+    })
+}
+
+#[inline(never)]
+pub fn wrap_on_width() {
+    let mut print_cfg = PRINT_CFG.write().unwrap();
+    let prev = print_cfg.take();
+
+    *print_cfg = Some(match prev {
+        Some(prev) => prev.width_wrapped(),
+        None => PrintCfg::default_for_files().width_wrapped(),
+    })
+}
+
+#[inline(never)]
+pub fn wrap_on_words() {
+    let mut print_cfg = PRINT_CFG.write().unwrap();
+    let prev = print_cfg.take();
+
+    *print_cfg = Some(match prev {
+        Some(prev) => prev.word_wrapped(),
+        None => PrintCfg::default_for_files().word_wrapped(),
+    })
+}
+
+#[inline(never)]
+pub fn wrap_on_cap(cap: usize) {
+    let mut print_cfg = PRINT_CFG.write().unwrap();
+    let prev = print_cfg.take();
+
+    *print_cfg = Some(match prev {
+        Some(prev) => prev.wrapped_on_cap(cap),
+        None => PrintCfg::default_for_files().wrapped_on_cap(cap),
+    })
+}
+
+#[inline(never)]
+pub fn indent_on_wrap() {
+    let mut print_cfg = PRINT_CFG.write().unwrap();
+    let prev = print_cfg.take();
+
+    *print_cfg = Some(match prev {
+        Some(prev) => prev.indenting_wrap(),
+        None => PrintCfg::default_for_files().indenting_wrap(),
+    })
+}
+
+#[inline(never)]
+pub fn tab_size(tab_size: usize) {
+    let mut print_cfg = PRINT_CFG.write().unwrap();
+    let prev = print_cfg.take();
+
+    *print_cfg = Some(match prev {
+        Some(prev) => prev.with_tabs_size(tab_size),
+        None => PrintCfg::default_for_files().with_tabs_size(tab_size),
+    })
+}
+
+#[inline(never)]
+pub fn new_line(char: char) {
+    let mut print_cfg = PRINT_CFG.write().unwrap();
+    let prev = print_cfg.take();
+
+    *print_cfg = Some(match prev {
+        Some(prev) => prev.with_new_line_as(char),
+        None => PrintCfg::default_for_files().with_new_line_as(char),
+    })
+}
+
+#[inline(never)]
+pub fn trailing_new_line(char: char) {
+    let mut print_cfg = PRINT_CFG.write().unwrap();
+    let prev = print_cfg.take();
+
+    *print_cfg = Some(match prev {
+        Some(prev) => prev.with_trailing_new_line_as(char),
+        None => PrintCfg::default_for_files().with_trailing_new_line_as(char),
+    })
+}
+
+#[inline(never)]
+pub fn scrolloff(x: usize, y: usize) {
+    let mut print_cfg = PRINT_CFG.write().unwrap();
+    let prev = print_cfg.take();
+
+    *print_cfg = Some(match prev {
+        Some(prev) => prev.with_scrolloff(x, y),
+        None => PrintCfg::default_for_files().with_scrolloff(x, y),
+    })
+}
+
+#[inline(never)]
+pub fn word_chars(word_chars: impl Iterator<Item = RangeInclusive<char>>) {
+    let mut print_cfg = PRINT_CFG.write().unwrap();
+    let prev = print_cfg.take();
+
+    *print_cfg = Some(match prev {
+        Some(prev) => prev.with_word_chars(word_chars),
+        None => PrintCfg::default_for_files().with_word_chars(word_chars),
+    })
+}
