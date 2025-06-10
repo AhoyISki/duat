bf8b154b973855c3ffabd89878d7e451871c75b7
AhoyISki <ahoyiski@gmail.com>
2025-06-10 20:04:30 -0300

    Fixed the default config.

--- config/src/lib.rs
+++ config/src/lib.rs
@@ -38,27 +38,27 @@ fn setup() {
     form::set("ExtraCursorGoTo", "ExtraCursorNormal");
 
     // The print module configures file printing.
-    print::wrap_on_width();
+    print::wrap_on_edge();
 
     //// Hooks
 
-    hooks::remove("FileWidgets");
+    hook::remove("FileWidgets");
     // This hook lets you push widgets to the files.
-    hooks::add::<OnFileOpen>(|builder| {
+    hook::add::<OnFileOpen>(|mut pa, builder| {
         // These widgets go on the left by default.
-        builder.push(VertRule::cfg());
-        builder.push(LineNumbers::cfg());
+        builder.push(&mut pa, VertRule::cfg());
+        builder.push(&mut pa, LineNumbers::cfg());
     });
 
-    hooks::remove("WindowWidgets");
+    hook::remove("WindowWidgets");
     // Same, but on the edges of the window.
-    hooks::add::<OnWindowOpen>(|builder| {
+    hook::add::<OnWindowOpen>(|mut pa, builder| {
         // // Uncomment this and comment the rest for one line
         // // StatusLine PromptLine combo
         // let (child, _) = buider.push(PromptLine::cfg());
-        // let status = status!(file_fmt " " mode_fmt " " selections_fmt " " main_fmt);
-        // builder.push_to(child, status.right_ratioed(4, 7));
-        // builder.push_to(child, Notifier::cfg());
+        // let status = status!(file_fmt " " mode_fmt " " selections_fmt " "
+        // main_fmt); builder.push_to(child, status.right_ratioed(4,
+        // 7)); builder.push_to(child, Notifier::cfg());
 
         // This function takes the mode and uppercases it.
         // The splitting is done to remove generic arguments.
@@ -69,21 +69,25 @@ fn setup() {
 
         // Pushes a StatusLine to the bottom
         // // Square bracket pairs change the Form of text.
-        builder.push(status!([Mode] mode_upper Spacer file_fmt " " selections_fmt " " main_fmt));
+        builder.push(
+            &mut pa,
+            status!("[Mode]{mode_upper}{Spacer}{file_fmt} {selections_fmt} {main_fmt}"),
+        );
         // Pushes a PromptLine to the bottom
-        let (child, _) = builder.push(PromptLine::cfg());
+        let (child, _) = builder.push(&mut pa, PromptLine::cfg());
         // By pushing this Notifications to the `child`, Duat will create a
-        // parent that owns only the PromptLine and Notifications widgets.
+        // parent that owns only the PromptLine and Notifier widgets.
         // With that, you can tell an Area to occupy the whole parent, "hiding
-        // other sibling Areas. That's done in, e.g., the "HidePromptLine" hook.
-        builder.push_to(child, Notifier::cfg());
+        // other sibling Areas. That's done in the "HidePromptLine" hook, for
+        // example.
+        builder.push_to(&mut pa, child, Notifier::cfg());
     });
     // // See what happens when you uncomment this hook removal:
-    // hooks::remove("HidePromptLine");
+    // hook::remove("HidePromptLine");
 
     // This hook will change the color of Kitty as
     // the ColorScheme is altered.
-    hooks::add::<ColorSchemeSet>(|scheme| {
+    hook::add::<ColorSchemeSet>(|_pa, scheme| {
         let scheme = match scheme {
             "catppuccin-latte" => "Catppuccin-Latte",
             "catppuccin-frappe" => "Catppuccin-Frappe",
@@ -104,19 +108,6 @@ fn setup() {
     // // You may see them by uncommenting this line:
     // form::set("Alias", Form::red());
 
-    //// Commands
-
-    // Adds a command for the LineNumbers widget.
-    cmd::add_for!(["toggle-relative", "tr"], |ln: LineNumbers<Ui>, _: Area| {
-        let opts = ln.options_mut();
-        opts.num_rel = match opts.num_rel {
-            LineNum::Abs => LineNum::RelAbs,
-            LineNum::Rel | LineNum::RelAbs => LineNum::Abs,
-        };
-        Ok(None)
-    })
-    .unwrap();
-
     form::set_colorscheme("catppuccin-mocha");
 }
 
@@ -125,18 +116,19 @@ fn setup() {
 fn file_fmt(file: &File) -> Text {
     if let Some(name) = file.name_set() {
         // A TextBuilder lets you build Text incrementally.
-        let mut b = Text::builder();
+        let mut builder = Text::builder();
         // [] pairs change the Form of the text
-        text!(b, [File] name);
+        builder.push(text!("[File]{name}"));
         if !file.exists() {
-            text!(b, [NewFile] "[new file]");
+            // Like in regular Rust formatting, double a "[" to escape it.
+            builder.push(text!("[NewFile][[new file]]"));
         } else if file.text().has_unsaved_changes() {
-            text!(b, [UnsavedChanges] "[+]");
+            text!("[UnsavedChanges][[+]]");
         }
-        b.finish()
+        builder.build()
     } else {
         // But you can also create Text directly
         // The second thing is a non identifier expression.
-        text!([ScratchFile] { file.name() })
+        text!("[ScratchFile]{}", file.name()).build()
     }
 }
