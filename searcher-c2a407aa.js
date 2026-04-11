4fd392924cde2cf152e6ac435fc0e3cb0ae69660
AhoyISki <AhoyISki@users.noreply.github.com>
2026-04-11 04:31:43 +0000

    deploy: a8f3aa77e05b38f924592b523bd39e6cb44f17d9

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-988232b1.js',
+                path_to_root + 'searchindex-bc1c0210.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
